2df50a1dad76117db4e254a963974ea94e58f8fd
James DV <azyriel14@gmail.com>
2015-03-13 23:45:37 +0800

    Added a unit test requiring the presence of a Type for a Pokemon.

--- test/models/pokemon_test.rb
+++ test/models/pokemon_test.rb
@@ -7,4 +7,11 @@ class PokemonTest < ActiveSupport::TestCase
 		p.name = nil
 		assert_not p.valid?, "pokemon should be invalid"
 	end
+
+	test "pokemon must have a type" do
+		p = Pokemon.first
+		assert p.valid?, "pokemon should be valid"
+		p.type = nil
+		assert_not p.valid?, "pokemon should be invalid"
+	end
 end
