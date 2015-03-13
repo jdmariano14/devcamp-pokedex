c5f2b0032ce213a367d458eb3efe9ffae33e3d79
James DV <azyriel14@gmail.com>
2015-03-14 01:26:42 +0800

    Revert last commit.
    Those validations were needed after all.

--- app/models/pokemon_move.rb
+++ app/models/pokemon_move.rb
@@ -1,5 +1,9 @@
 class PokemonMove < ActiveRecord::Base
 	########################################
+	# Validations
+	########################################
+	validates_uniqueness_of :move_id, :scope => :pokemon_id
+	########################################
 	# Associations
 	########################################
 	belongs_to :pokemon
