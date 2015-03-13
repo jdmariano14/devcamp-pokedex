6723d59b5d73ebffcb307802383a196e4e790fc6
James DV <azyriel14@gmail.com>
2015-03-13 23:15:17 +0800

    Enforced many-to-many association between Pokemon and Moves.

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,3 +1,6 @@
 class Move < ActiveRecord::Base
 	belongs_to :type
+
+	has_many :pokemon_moves
+	has_many :pokemons, :through => :pokemon_moves
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,6 @@
 class Pokemon < ActiveRecord::Base
 	belongs_to :type
+
+	has_many :pokemon_moves
+	has_many :moves, :through => :pokemon_moves
 end
