e50c2f4a2416ea85b51341e9166c71d7502dd045
James DV <azyriel14@gmail.com>
2015-03-13 23:41:33 +0800

    Added a unit test requiring the presence of a name for a Pokemon.

--- test/models/pokemon_test.rb
+++ test/models/pokemon_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PokemonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "pokemon must have a name" do
+		p = Pokemon.first
+		assert p.valid?, "pokemon should be valid"
+		p.name = nil
+		assert_not p.valid?, "pokemon should be invalid"
+	end
 end
