538d9a204059c7b4d60d93c6a6f686a32f0e97fa
James DV <azyriel14@gmail.com>
2015-03-14 00:31:56 +0800

    Added a unit test for checking the uniqueness of each move in a Pokemon's move set.

--- test/models/pokemon_move_test.rb
+++ test/models/pokemon_move_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PokemonMoveTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "pokemon cannot have more than one of the same move" do
+		m1 = PokemonMove.first
+		m2 = PokemonMove.new
+		m2.pokemon_id = m1.pokemon_id
+		m2.move_id = m1.move_id + 1
+		assert m2.valid?, "pokemon_move should be valid"
+		m2.move_id = m1.pokemon_id
+		assert_not m2.valid?, "pokemon_move should be invalid"
+	end
 end
