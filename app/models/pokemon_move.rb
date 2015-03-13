577a725da08b9ee3e3427d78d020aa99bc14ed90
James DV <azyriel14@gmail.com>
2015-03-14 00:35:54 +0800

    Added a validation in the Pokemon Move model to pass the test for uniqueness.

--- app/models/pokemon_move.rb
+++ app/models/pokemon_move.rb
@@ -1,4 +1,11 @@
 class PokemonMove < ActiveRecord::Base
+	########################################
+	# Validations
+	########################################
+	validates_uniqueness_of :move_id, :scope => :pokemon_id
+	########################################
+	# Associations
+	########################################
 	belongs_to :pokemon
 	belongs_to :move
 end
--- test/models/pokemon_move_test.rb
+++ test/models/pokemon_move_test.rb
@@ -7,7 +7,7 @@ class PokemonMoveTest < ActiveSupport::TestCase
 		m2.pokemon_id = m1.pokemon_id
 		m2.move_id = m1.move_id + 1
 		assert m2.valid?, "pokemon_move should be valid"
-		m2.move_id = m1.pokemon_id
+		m2.move_id = m1.move_id
 		assert_not m2.valid?, "pokemon_move should be invalid"
 	end
 end
