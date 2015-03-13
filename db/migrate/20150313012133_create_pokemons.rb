f12b1e291ed2f2709a1f09d51b923864b57c08e2
James DV <azyriel14@gmail.com>
2015-03-13 22:56:55 +0800

    Added a down method to the Pokemon migration for future rollbacks.

--- db/migrate/20150313012133_create_pokemons.rb
+++ db/migrate/20150313012133_create_pokemons.rb
@@ -1,10 +1,14 @@
 class CreatePokemons < ActiveRecord::Migration
-  def change
+  def up
     create_table :pokemons do |t|
       t.string :name
       t.integer :type_id
       
       t.timestamps null: false
     end
+  end
+
+  def down
+  	drop_table :pokemons
   end
 end
